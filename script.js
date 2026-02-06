e3b7955c44d81c5ed9a783bd2da94f2d5218beb3
gmhs0000-ops <gmhs0000@proton.me>
2026-02-06 10:28:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-alert("bobr")
-alert(3)
+// alert("bobr")
+// alert(3)
 
 let userName = "Noe";
 console.log(userName);
+
+userName = "Philipp Polívka";
+console.log(userName)
+
+console.log(7+4);
+console.log("7"+"4")
+console.log("7" + 4)
