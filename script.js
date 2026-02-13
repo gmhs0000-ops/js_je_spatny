193bcbba0d126895a707d70018e8302cf0374f67
gmhs0000-ops <gmhs0000@proton.me>
2026-02-13 10:37:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ console.log(userName)
 console.log(7+4);
 console.log("7"+"4")
 console.log("7" + 4)
+
+let aage = 31;
+console.log(aage > 20)
+
+let text = "Text";
+console.log(typeof text)
+
+let hodnota = null;
+console.log(hodnota)
+
+let cisloA = 10;
+let cisloB = 5;
+console.log(cisloA + cisloB + " = expired")
+
+console.log(cisloA + cisloB)
+console.log(cisloA - cisloB)
+console.log(cisloA * cisloB)
+console.log(cisloA / cisloB)
+console.log(cisloA ** cisloB)
+console.log(cisloA % cisloB)
+console.log(cisloA + cisloB * cisloB)
+console.log((cisloA + cisloB) * cisloB)
+
+let age = 15;
+
+if (age > 12) {
+  console.log("expired");
+}
+
+if (age < 10) {
+  console.log("FRESH");
+}
+
+if (age < 5) {
+  console.log("E F N");
+}
